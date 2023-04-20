f21b3c5b74bf276c034153a7cb9cff3bf52821ed
Anton <antonsnizhko29@gmail.com>
2023-04-20 01:26:26 -0500

    ok
    yes

---
+++ src/murach/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Anton Snizhko
+ * Chapter 21-1 assignment
+ * Nov 8, 2022
+ */
+package murach;
+
+import murach.ui.ProductManagerFrame;
+
+public class Main {
+
+    @SuppressWarnings("unused")
+	public static void main(String[] args) {
+        ProductManagerFrame frame = new ProductManagerFrame();
+    }
+}
---
+++ src/murach/db/DBException.java
@@ -0,0 +1,19 @@
+/**
+ * Anton Snizhko
+ * Chapter 21-1 assignment
+ * Nov 8, 2022
+ */
+package murach.db;
+
+/* 
+ * This is just a wrapper class so we can throw a common exception for
+ * the UI to catch without tightly coupling the UI to the database layer.
+ */
+@SuppressWarnings("serial")
+public class DBException extends Exception {
+    DBException() {}
+    
+    DBException(Exception e) {
+        super(e);
+    }
+}
